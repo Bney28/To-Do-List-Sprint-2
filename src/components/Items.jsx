4a92cc6594c4b253d8201378ea315ac703238f29
Bney28 <bney1@outlook.com>
2023-01-14 09:49:27 -0500

    feat: added div-ppal

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -4,12 +4,14 @@ import "../styles/CSSItems.css"
 const Items = () => {
     return (
         <>
-            <div className="cont-item">
-                <div className="cont-item__container">
-                    <i class="fa-regular fa-circle-check cont-item__icon"></i>
-                    <p className="cont-item__item">Example</p>
-                </div>
+            <div className="div-ppal">
+                <div className="cont-item">
+                    <div className="cont-item__container">
+                        <i class="fa-regular fa-circle-check cont-item__icon"></i>
+                        <p className="cont-item__item">Example</p>
+                    </div>
                     <i className="fa-solid fa-xmark cont-item__close"></i>
+                </div>
             </div>
         </>
     )
