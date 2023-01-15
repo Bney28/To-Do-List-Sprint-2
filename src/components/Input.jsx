a65baaad1baa0926dc7c8b6705eb7005ae838fe6
Bney28 <bney1@outlook.com>
2023-01-15 15:59:02 -0500

    feat: new functions

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,34 +1,48 @@
 import React, { useState } from 'react'
 import "../styles/CSSInput.css"
+import { v4 as uuidv4 } from 'uuid'  //Generador de Id's
 
-const Input = ({newToDo}) => {
-    const [text, setText] = useState("")
-    
-    const handleInput = (e) =>{
-        if (e.key.toLowerCase() === 'enter') {
+const Input = (props) => {
+    const [input, setInput] = useState("")
+
+    const handleChange = (e) => {    //Función para manejar cambios, manejo de la info
+        setInput(e.target.value)
+    }
+
+    const handleSend = (e) => {    // Función para manejar el envío del formulario
+        if (e.key === "Enter") {
             e.preventDefault()
-            newToDo(text)
-            setText("")
+
+            const newTodo = {      //Objeto que crea la nueva tarea
+                id: uuidv4(),
+                text: input,
+                completed: false
+            }
+
+            props.onSubmit(newTodo)
+            setInput('')
         }
     }
 
-    /* console.log(handleInput); */
 
     return (
         <>
-            <form className="form">
+            <form
+                className="form"
+                onKeyDown={handleSend}
+            >
                 <i className="fa-regular fa-circle form__icon"></i>
                 <input
-                    value={text} type="text"
+                    value={input}
+                    type="text"
                     className="form__input"
                     placeholder="New to do..."
-                    onChange={e => setText(e.target.value)}
-                    onKeyDown={e => handleInput(e)}
-                    />
+                    onChange={handleChange}
+                />
             </form>
         </>
     )
-    
+
 }
 
 export default Input
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import "../styles/CSSItems.css"
 
-const Items = ({todo}) => {
+//Pendiente estilizar clase completed
 
-    const {text} = {todo}
+const Items = ({ id, text, completed, toDoRealized, deleteToDo }) => {
 
     return (
         <>
-            <div className="cont-item">
-                <div className="cont-item__container">
+            <div className={completed ? "cont-item completed" : "cont-item"}> {/* Ternario condicional para completed*/}
+                <div
+                    className="cont-item__container"
+                    onClick={() => toDoRealized(id)}
+                >
                     <i class="fa-regular fa-circle-check cont-item__icon"></i>
                     <p className="cont-item__item">
                         {text}
                     </p>
                 </div>
-                <i className="fa-solid fa-xmark cont-item__close"></i>
+                <i
+                    className="fa-solid fa-xmark cont-item__close"
+                    onClick={() => deleteToDo(id)}
+                ></i>
             </div>
         </>
     )
